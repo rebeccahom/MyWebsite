0cac6fd9f08510d44e4e0a5636664865471ec6dc
Rebecca Hom <rebecca.hom@nyu.edu>
2018-07-20 11:19:52 -0400

    Nav css makeover

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -7,10 +7,17 @@ class NavBar extends React.Component {
             <div className="navbar">
                 <div className="navbar-menu">
                     <div className="navbar-start">
-                        <a className="navbar-item" href="/">Home</a>
-                        <a className="navbar-item" href="/about">About</a>
-                        <a className="navbar-item" href="/portfolio" >Portfolio (coming soon)</a>
-                        <a className="navbar-item" href="/contact">Contact</a>
+                        <a className="navbar-item" href="/">HOME</a>
+                        <a className="navbar-item" href="/about">ABOUT</a>
+                        <a className="navbar-item" href="#" >PORTFOLIO</a>
+                    </div>
+                    <div className="navbar-end">
+                        <a className="navbar-item navbarIcon" href="https://github.com/rebeccahom">
+                            <img src={require('../images/githubIcon.png')}/>
+                        </a>
+                        <a className="navbar-item navbarIcon" href="https://www.linkedin.com/in/rebecca-hom-683a00102">
+                            <img src={require('../images/linkedInIcon.png')}/>
+                        </a>
                     </div>
                 </div >
             </div>
