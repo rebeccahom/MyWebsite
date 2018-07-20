d4bbfdeb3c15c000cfe2ad98645b9709b18d8ba3
Rebecca Hom <rebecca.hom@nyu.edu>
2018-07-20 11:18:02 -0400

    Updated about content

--- src/Components/AboutContent.tsx
+++ src/Components/AboutContent.tsx
@@ -5,17 +5,21 @@ class AboutContent extends React.Component {
     render() {
         return (
             <div className="aboutContent">
-                <p>I am currently a junior at NYU in the College of Arts and Science majoring
+                <p>I am currently a senior at NYU in the College of Arts and Science majoring
                     in Computer Science and minoring in Web Programming and Applications.
                     Here is my coding experience: </p>
                 <ul>
-                    <li>Two years of Java</li>
-                    <li>Two years of HTML/CSS</li>
-                    <li>A little experience coding in Python, C, JavaScript, jQuery, PHP, and MySQL</li>
+                    <li>Proficient in Java, JavaScript and React/Redux</li>
+                    <li>Fairly adept in HTML/CSS, TypeScript, and Processing</li>
+                    <li>Familiar with Python, jQuery, PHP, and MySQL</li>
                 </ul>
-                <p>I also have some graphic design experience - I am familiar with Adobe Illustrator
-                    and have made graphics that can be found in my portfolio.
-            <br /><br />If you would like to see my resume, please inquire via my Contact page. Thank you!</p>
+
+                <p>I have experience working with git and IDEs such as Visual Studio Code,
+                    Eclipse, Sublime, and Atom.
+                </p>
+                <p>I also have some graphic design experience - my graphics can be found in my portfolio as well as
+                    my coding projects.</p>
+                <p>If you would like to see my resume, please inquire via my Contact page. Thank you!</p>
             </div>
         );
     }
