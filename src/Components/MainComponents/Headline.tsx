b2a2aa742ecff938ac52d3c49a430dc2843cb810
Rebecca Hom <rebecca.hom@nyu.edu>
2018-08-27 11:17:38 -0400

    Fixed highlighting problem

--- src/Components/MainComponents/Headline.tsx
+++ src/Components/MainComponents/Headline.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import '../../Styles/Headline.css';
+import Landscape from './Landscape';
 
 class Headline extends React.Component {
     render() {
         return (
-            <div className="center" id="bigTitleFadeIn">
-                <h1>Hi, I'm Rebecca.</h1>
-                <h3>Student // Programmer // Designer</h3>
-                <p id="smallTitleFadeIn">Click on one of the links above to get started.</p>
-                <div id="heart"/>
+            <div>
+                <div className="center" id="bigTitleFadeIn">
+                    <h1>Hi, I'm Rebecca.</h1>
+                    <h3>Student // Programmer // Designer</h3>
+                    <p id="smallTitleFadeIn">Click on one of the links above to get started.</p>
+                    <div id="heart"/>
+                </div>
+                <Landscape/>
             </div>
         );
     }
