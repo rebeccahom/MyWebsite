b431c39bcdc6a223d9bcfbba48bbc0a80f6baf85
Rebecca Hom <rebecca.hom@nyu.edu>
2018-10-05 13:18:28 -0400

    Fixed p5 sketch with about page + added circle styles

--- src/Components/AboutComponents/AboutMain.tsx
+++ src/Components/AboutComponents/AboutMain.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
-import Education from './Education';
+import '../../Styles/About.css';
+/*import Education from './Education';
 import Languages from './Languages';
 import IDEs from './IDEs';
 import Other from './Other';
-import Hobbies from './Hobbies';
+import Hobbies from './Hobbies';*/
 
 class AboutContent extends React.Component {
     render() {
         return (
-            <div>
-                <Education />
-                <Languages />
-                <IDEs />
-                <Other />
-                <Hobbies />
+            <div id='showCircles'>
+                <div className='flex'>
+                    <div className='circle' style={{backgroundColor: "#f48a78"}}><p className='centerCircleLabel'>EDUCATION</p></div>
+                    <div className='circle' style={{backgroundColor: "#eda277"}}><p className='centerCircleLabel'>LANGUAGES</p></div>
+                    <div className='circle' style={{backgroundColor: "#f6c98a"}}><p className='centerCircleLabel'>IDEs</p></div>
+                </div>
+                <div className='flex'>
+                    <div className='circle' style={{backgroundColor: "#e1d99c"}}><p className='centerCircleLabel'>OTHER</p></div>
+                    <div className='circle' style={{backgroundColor: "#bfead3"}}><p className='centerCircleLabel'>HOBBIES</p></div>
+                </div>
             </div>
         );
     }
