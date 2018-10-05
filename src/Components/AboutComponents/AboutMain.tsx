7b189169756706523c84903378de6677c6fc49e7
Rebecca Hom <rebecca.hom@nyu.edu>
2018-10-05 14:56:02 -0400

    Added bounce animation on hover

--- src/Components/AboutComponents/AboutMain.tsx
+++ src/Components/AboutComponents/AboutMain.tsx
@@ -11,13 +11,18 @@ class AboutContent extends React.Component {
         return (
             <div id='showCircles'>
                 <div className='flex'>
-                    <div className='circle' style={{backgroundColor: "#f48a78"}}><p className='centerCircleLabel'>EDUCATION</p></div>
-                    <div className='circle' style={{backgroundColor: "#eda277"}}><p className='centerCircleLabel'>LANGUAGES</p></div>
-                    <div className='circle' style={{backgroundColor: "#f6c98a"}}><p className='centerCircleLabel'>IDEs</p></div>
+                    <div className='circle bounceAnimation' style={{backgroundColor: "#f48a78"}}>
+                        <p className='centerCircleLabel'>EDUCATION</p></div>
+                    <div className='circle bounceAnimation' style={{backgroundColor: "#eda277"}}>
+                        <p className='centerCircleLabel'>LANGUAGES</p></div>
+                    <div className='circle bounceAnimation' style={{backgroundColor: "#f6c98a"}}>
+                        <p className='centerCircleLabel'>IDEs</p></div>
                 </div>
                 <div className='flex'>
-                    <div className='circle' style={{backgroundColor: "#e1d99c"}}><p className='centerCircleLabel'>OTHER</p></div>
-                    <div className='circle' style={{backgroundColor: "#bfead3"}}><p className='centerCircleLabel'>HOBBIES</p></div>
+                    <div className='circle bounceAnimation' style={{backgroundColor: "#e1d99c"}}>
+                        <p className='centerCircleLabel'>OTHER</p></div>
+                    <div className='circle bounceAnimation' style={{backgroundColor: "#bfead3"}}>
+                        <p className='centerCircleLabel'>HOBBIES</p></div>
                 </div>
             </div>
         );
