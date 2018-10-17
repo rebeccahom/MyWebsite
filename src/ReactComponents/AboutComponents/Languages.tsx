dda10d3b0b21e360e524e8171706aaa73c17b752
Rebecca Hom <rebecca.hom@nyu.edu>
2018-10-17 14:11:38 -0400

    Added more Languages in About

--- src/ReactComponents/AboutComponents/Languages.tsx
+++ src/ReactComponents/AboutComponents/Languages.tsx
@@ -4,7 +4,7 @@ import '../../Styles/Languages.css';
 class Languages extends React.Component {
     proficientBox() {
         return (
-            <div id='proficientBox'>
+            <div className='toolbox'>
                 <div className='rows'>
                     <div className='container'>
                         <div className='overlay'><p>Java</p></div>
@@ -34,6 +34,48 @@ class Languages extends React.Component {
                     <img src={require('../../images/TypeScriptLogo.png')} className='imgSizing'/>
                 </div>
                 </div>
+
+                <div className='rows'>
+                <div className='container'>
+                    <div className='overlay'><p>HTML5</p></div>
+                    <img src={require('../../images/HtmlLogo.png')} className='imgSizing'/>
+                </div>
+                <div className='container'>
+                    <div className='overlay'><p>CSS3</p></div>
+                    <img src={require('../../images/CSSLogo.png')} className='imgSizing'/>
+                </div>
+                <div className='container'/>
+                </div>
+            </div>
+        );
+    }
+
+    beginnerBox() {
+        return (
+            <div className='toolbox'>
+                <div className='rows'>
+                    <div className='container'>
+                        <div className='overlay'><p>Processing</p></div>
+                        <img src={require('../../images/processing.png')} className='imgSizing'/>
+                    </div>
+                    <div className='container'>
+                        <div className='overlay'><p>jQuery</p></div>
+                        <img src={require('../../images/jQueryLogo.png')} className='imgSizing'/>
+                    </div>
+                    <div className='container'>
+                        <div className='overlay'><p>PHP</p></div>
+                        <img src={require('../../images/PHPLogo.png')} className='imgSizing'/>
+                    </div>
+                </div>
+
+                <div className='rows'>
+                    <div className='container'>
+                        <div className='overlay'><p>MySQL</p></div>
+                        <img src={require('../../images/sql.png')} className='imgSizing'/>
+                    </div>
+                    <div className='container'/>
+                    <div className='container'/>
+                </div>
             </div>
         );
     }
@@ -41,8 +83,13 @@ class Languages extends React.Component {
     render() {
         return (
             <div id="Languages">
-            <h3>Proficient Toolbox</h3>
+                <h3>Proficient Toolbox</h3>
+                <h4>Languages that I'm proficient/advanced in</h4>
                 {this.proficientBox()}
+
+                <h3>Beginner Toolbox</h3>
+                <h4>Languages that I've had exposure to and/or worked with at some point in the past</h4>
+                {this.beginnerBox()}
             </div>
         );
     }
